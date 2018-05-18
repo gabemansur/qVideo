9256febb57e450050a252f61ce6fd8d5777a8eec
Gabe Mansur <gabe_mansur@harvard.edu>
2018-05-18 16:04:00 -0400

    Initial commit

---
+++ js/qVideo.js
@@ -0,0 +1,11 @@
+
+/* Feature detection */
+function hasGetUserMedia() {
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+}
+
+if (hasGetUserMedia()) {
+  // Good to go!
+} else {
+  alert('getUserMedia() is not supported by your browser');
+}
